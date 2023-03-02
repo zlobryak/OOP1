b6aa9867aa1f426a5547a7f900a3227f90d2c165
Neto Homework <clychnik@gmail.com>
2023-03-02 18:58:27 +0300

    Braces in main fix

--- src/main/java/ru/netology/oop/Radio.java
+++ src/main/java/ru/netology/oop/Radio.java
@@ -4,55 +4,63 @@ public class Radio {
     private int radioStationNumber;
     private int currentVolume = 5;
 
-    public void setRadioStationNumber(int newStationNumber){
+    public void setRadioStationNumber(int newStationNumber) {
         if (newStationNumber >= 0)
-            if (newStationNumber <= 9) radioStationNumber = newStationNumber;
-        else radioStationNumber = 0;
+            if (newStationNumber <= 9) {
+                radioStationNumber = newStationNumber;
+            } else {
+                radioStationNumber = 0;
+            }
     }
-    public void next(){
+
+    public void next() {
         if (radioStationNumber < 9) {
             radioStationNumber++;
-        }
-        else {
+        } else {
             radioStationNumber = 0;
         }
     }
 
-    public void prev(){
+    public void prev() {
         if (radioStationNumber > 0) {
             radioStationNumber--;
-
-        }
-        else  {
+        } else {
             radioStationNumber = 9;
         }
 
     }
-    public int getRadioStationNumber(){
 
+    public int getRadioStationNumber() {
         return radioStationNumber;
     }
 
     public void increaseVolume() {
         if (currentVolume < 10) {
-            currentVolume ++;
+            currentVolume++;
         }
     }
+
     public void decreaseVolume() {
         if (currentVolume > 0) {
-            currentVolume --;
+            currentVolume--;
         }
     }
-    public void setRadioVolume(int newRadioVolume){ // Used only for tests.
+
+    public void setRadioVolume(int newRadioVolume) { // Used only for tests.
 
         if (newRadioVolume >= 0) {
-            if (newRadioVolume <= 10){
+            if (newRadioVolume <= 10) {
                 currentVolume = newRadioVolume;
-            } else currentVolume = 0;
-        } else currentVolume = 0;
+            } else {
+                currentVolume = 0;
+            }
+        } else {
+            currentVolume = 0;
+        }
 
     }
-    public  int getRadioVolume(){
+
+    public int getRadioVolume() {
         return currentVolume;
     }
 }
